3aaf02f37835922996a53f49509f542ee8e9a57b
Eslam Ashraf <eslam.ashraf@badrit.com>
2015-04-12 20:52:52 -0700

    final solution

--- MySolution/CubicSpline.py
+++ MySolution/CubicSpline.py
@@ -5,12 +5,12 @@ import numpy as numpy
 import pylab
 
 # DataSet #1
-data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_train.txt")
-testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_test.txt")
+# data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_train.txt")
+# testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_test.txt")
 
 # DataSet #2
-# data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_train.txt")
-# testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_test.txt")
+data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_train.txt")
+testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_test.txt")
 
 X = [a[0] for a in data]
 Y = [a[1] for a in data]
@@ -18,21 +18,26 @@ basisVector = [[1, a, pow(a,2), pow(a,3), pow(max(0,a+1),3), pow(max(0,a),3), po
 
 w, residuals, _, _ = numpy.linalg.lstsq(basisVector, Y)
 
-# printing
+# printing Weight Vector
+# ======================
 print "Weight Vector"
 print w
-print ("MSE: %f" %residuals[0])
 
+# Calculate MSE for Training Data
+# ================================
+trainMSE = residuals/len(X)
+print ("MSE for training data: %f" %(trainMSE))
 
-# Test Data
-# =========
+# Calculate MSE for Testing Data
+# =============================
 testX = [a[0] for a in testdata]
-expectedY = [a[1] for a in testdata]
+testY = [a[1] for a in testdata]
 predictedY = [w[0]+ w[1]*x + w[2]*pow(x,2) + w[3]*pow(x,3) + w[4]*pow(max(0,x+1),3) + w [5]*pow(max(0,x),3) + w[6]*pow(max(0,x-2),3) for x in testX]
 
 testMSE = 0.0
-for i in range(0, len(testX)-1):
-    testMSE += pow(predictedY[i] - expectedY[i], 2)
+for i in range(0, len(testX)):
+    testMSE += pow(predictedY[i] - testY[i], 2)
+testMSE /= len(testX)
 print ("MSE for test data: %f" %testMSE)
 
 
@@ -44,10 +49,10 @@ pyplot.subplot(1, 2, 1)
 pyplot.plot(X, Y, "ro")
 modeloutput = [w[0]+ w[1]*x + w[2]*pow(x,2) + w[3]*pow(x,3) + w[4]*pow(max(0,x+1),3) + w [5]*pow(max(0,x),3) + w[6]*pow(max(0,x-2),3)  for x in range]
 pyplot.plot(range, modeloutput)
-pyplot.title("Training Data: MSE = %f" % residuals[0])
+pyplot.title("Training Data: MSE = %f" % trainMSE)
 
 pyplot.subplot(1, 2, 2)
-pyplot.plot(testX, expectedY, "ro")
+pyplot.plot(testX, testY, "ro")
 pyplot.plot(range, modeloutput)
 pyplot.title("Testing Data: MSE = %f" % testMSE)
 
--- MySolution/LinearRegression.py
+++ MySolution/LinearRegression.py
@@ -5,12 +5,12 @@ import numpy as numpy
 import pylab
 
 # DataSet #1
-data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_train.txt")
-testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_test.txt")
+# data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_train.txt")
+# testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample1_test.txt")
 
 # DataSet #2
-# data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_train.txt")
-# testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_test.txt")
+data = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_train.txt")
+testdata = numpy.loadtxt("/Users/eslamelsawy/Desktop/ML Course/Course 2/Hw #1/HW1_sample_data/hw1_sample2_test.txt")
 
 X = [a[0] for a in data]
 Y = [a[1] for a in data]
@@ -18,22 +18,26 @@ basisVector = [[1, a] for a in X]
 
 w, residuals, _, _ = numpy.linalg.lstsq(basisVector, Y)
 
-# printing output
-# ===============
+# printing Weight Vector
+# ======================
 print "Weight Vector"
 print w
-print ("MSE for training data: %f" %residuals[0])
 
+# Calculate MSE for Training Data
+# ================================
+trainMSE = residuals/len(X);
+print ("MSE for training data: %f" %(trainMSE))
 
-# Test Data
-# =========
+# Calculate MSE for Testing Data
+# =============================
 testX = [a[0] for a in testdata]
-expectedY = [a[1] for a in testdata]
+testY = [a[1] for a in testdata]
 predictedY = [w[0]+w[1]*x for x in testX]
 
 testMSE = 0.0
-for i in range(0, len(testX)-1):
-    testMSE += pow(predictedY[i] - expectedY[i], 2)
+for i in range(0, len(testX)):
+    testMSE += pow(predictedY[i] - testY[i], 2)
+testMSE /= len(testX)
 print ("MSE for test data: %f" %testMSE)
 
 
@@ -44,14 +48,14 @@ range = numpy.arange(-5,5,0.1)
 pyplot.subplot(1, 2, 1)
 pyplot.plot(X, Y, "ro")
 pyplot.plot(range, w[0]+w[1]*range)
-pyplot.title("Training Data: MSE = %f" % residuals[0])
+pyplot.title("Training Data: MSE = %f" % trainMSE)
 
 pyplot.subplot(1, 2, 2)
-pyplot.plot(testX, expectedY, "ro")
+pyplot.plot(testX, testY, "ro")
 pyplot.plot(range, w[0]+w[1]*range)
 pyplot.title("Testing Data: MSE = %f" % testMSE)
 
-pyplot.show();
+pyplot.show()
 
 
 
